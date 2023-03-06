fecf53ef0fcdce048601503bc4d55b1f0c85eb12
Gabriel <devpalhares@gmail.com>
2023-03-06 14:14:41 -0300

    feat: requisito 3

--- src/migrations/20230303175728-create-posts_categories.js
+++ src/migrations/20230303175728-create-posts_categories.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const PostsCategoriesTable = queryInterface.createTable('posts_categories', {
-      id: {
+      post_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
